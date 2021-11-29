33139419ed561e1061f2b2a666d2c77af8044272
Beergrizl <a.sadovskii@tut.by>
2021-11-29 21:15:52 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Header from "./header/Header";
 import Main from "./main/Main";
 import Skills from "./skills/Skills";
-import Progects from "./progects/projects";
 import JobOffer from "./job offer/jobOffer";
 import Contacts from "./contacts/contacts";
 import Footer from "./footer/footer";
+import {Route, Routes} from "react-router-dom";
+import Projects from "./progects/projects";
 
 
 function App() {
@@ -15,10 +16,18 @@ function App() {
             <Header/>
             <Main/>
             <Skills/>
-            <Progects/>
+            <Projects/>
             <JobOffer/>
             <Contacts/>
             <Footer/>
+            {/*<Routes>
+                <Route path='/Main' element={<Main/>}/>
+                <Route path='/Skills' element={<Skills/>}/>
+                <Route path='/Projects' element={<Projects/>}/>
+                <Route path='/Contacts' element={<JobOffer/>}/>
+                <Route path='/Contacts' element={<Contacts/>}/>
+                <Route path='/Contacts' element={<Footer/>}/>
+            </Routes>*/}
         </div>
     );
 }
--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,13 +1,15 @@
 import React from "react";
 import s from './Nav.module.scss';
 
+
 function Nav() {
+
     return (
         <div className={s.nav}>
-<a href=''>Main</a>
-<a href=''>Skills</a>
-<a href=''>Progects</a>
-<a href=''>Contacts</a>
+            <a href='/Main'>Main</a>
+            <a href='/Skills'>Skills</a>
+            <a href='/Projects'>Projects</a>
+            <a href='Contacts'>Contacts</a>
         </div>
     );
 }
